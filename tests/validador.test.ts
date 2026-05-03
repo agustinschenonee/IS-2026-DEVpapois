3a3b92e3b09f5afa505bafbac54cdc44ec917bbe
priiscigaleano <priiscigaleano@gmail.com>
2026-05-03 16:12:00 -0300

    Update validador.test.ts

--- tests/validador.test.ts
+++ tests/validador.test.ts
@@ -1,5 +1,7 @@
+import { describe, test, expect } from 'vitest';
+
 describe('Pruebas unitarias de DEVpapois', () => {
-  test('Verificar que el entorno de CI/CD reconoce TypeScript', () => {
+  test('Verificar que el entorno de CI/CD reconoce TypeScript con Vitest', () => {
     const proyecto: string = "DEVpapois";
     expect(proyecto).toBe("DEVpapois");
   });
