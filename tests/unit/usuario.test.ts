57a7d1e2aed908a21880a92ce8de80fbdb6b6f4c
priiscigaleano <priiscigaleano@gmail.com>
2026-05-03 23:29:59 -0300

    Update usuario.test.ts

--- tests/unit/usuario.test.ts
+++ tests/unit/usuario.test.ts
@@ -1,18 +1,19 @@
-// tests/unit/usuario.test.ts
 import { describe, it, expect } from 'vitest';
+import { Usuario } from '../../src/logic/Usuario'; 
+
 describe('Pruebas de Registro de Usuario - DEVpapois', () => {
   it('Debe validar un registro con nombre de 2 letras y clave de 4', () => {
     const user = new Usuario(null, 'Pr', 'pri@ucp.edu.ar', '1234');
-    expect(user.validarRegistro()).toBe(true); // Ahora esto debe pasar
+    expect(user.validarRegistro()).toBe(true);
   });
 
   it('Debe rechazar un nombre de menos de 2 caracteres', () => {
     const user = new Usuario(null, 'A', 'avril@test.com', '1234');
-    expect(user.validarRegistro()).toBe(false); // Falla por nombre corto
+    expect(user.validarRegistro()).toBe(false);
   });
 
   it('Debe rechazar una contraseña de menos de 4 caracteres', () => {
     const user = new Usuario(null, 'Itati', 'itati@test.com', '123');
-    expect(user.validarRegistro()).toBe(false); // Falla por clave corta
+    expect(user.validarRegistro()).toBe(false);
   });
 });
