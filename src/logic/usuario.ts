eaf5612749059e6125f1736db1e614b116e81b6d
priiscigaleano <priiscigaleano@gmail.com>
2026-05-03 23:02:38 -0300

    Create usuario.ts

---
+++ src/logic/usuario.ts
@@ -0,0 +1,19 @@
+export class usuario {
+  constructor(
+    public id: string | null,
+    public nombre: string,
+    public email: string,
+    public password: string,
+    public rol: 'admin' | 'cliente' = 'cliente' // Por defecto son clientes
+  ) {}
+
+  // Acá van a ir las validaciones que el robot va a testear
+  validarRegistro(): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return (
+      this.nombre.length >= 3 && 
+      emailRegex.test(this.email) && 
+      this.password.length >= 8
+    );
+  }
+}
