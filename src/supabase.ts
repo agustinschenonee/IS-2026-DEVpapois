75762d7b50dd6f4068cadd3b18bd501af85e18bf
priiscigaleano <priiscigaleano@gmail.com>
2026-05-04 10:48:45 -0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// URL de DEVpapois
-const supabaseUrl = 'https://wybiblrhcphwuhrmgkhv.supabase.co';
-
-// Usamos la nueva clave publishable (es la versión moderna de la 'anon' key)
-const supabaseKey = 'sb_publishable_tP0l_3gGWpQYnxci-Dn_-w_wdbl5AhM';
+// Archivo: src/supabase.ts
+const supabaseUrl = 'https://wybiblrhcphwuhrmgkhv.supabase.co'; 
+const supabaseKey = 'sb_publishable_tP0l_3gGWpQYnxci-Dn_-w_wdbl5AhM'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
