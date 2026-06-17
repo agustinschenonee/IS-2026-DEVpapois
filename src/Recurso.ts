c94b26577df1ddc3a7e648038808eb073f9cd19e
valen <vcarabajal94@gmail.com>
2026-06-17 19:42:51 -0300

    Add amenities property to Recurso interface

--- src/Recurso.ts
+++ src/Recurso.ts
@@ -5,8 +5,10 @@ export interface Recurso {
     tipo: string;
     imagen_url?: string;
     descripcion?: string;
+    amenities?: string[];
     disponible: boolean;
     mantenimiento?: string;
-    horario_disponible?: string; // <--- Nuevo
+    horario_disponible?: string;
     obtenerDetalles(): string;
 }
+
