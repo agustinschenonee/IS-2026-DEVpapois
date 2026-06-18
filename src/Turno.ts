f105a9fd89170305a3b4186f4c296b4e7842603d
nicoaguilera18 <nicolasaguilera7914@gmail.com>
2026-06-17 23:55:23 -0300

    Delete src/Turno.ts

--- src/Turno.ts
+++
@@ -1,9 +0,0 @@
-export class Turno {
-    constructor(
-        public id: number,
-        public recursoId: number,
-        public usuario: string,
-        public fecha: string,
-        public estado: string = 'CONFIRMADO'
-    ) {}
-}
