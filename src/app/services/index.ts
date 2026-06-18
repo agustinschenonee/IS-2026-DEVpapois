c386af1fd2d03fa77d51fa39da4eb63a83ee6396
nicoaguilera18 <nicolasaguilera7914@gmail.com>
2026-06-18 00:25:13 -0300

    Delete src/app/services/index.ts

--- src/app/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './supabase';
-export * from './AuthService';
-export * from './AdminService';
-export * from './BloqueoService';
-export * from './TurnoService';
-export * from './ValidadorOcupacion';
-export * from './adapters';
