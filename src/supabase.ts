3e717b688b87c63b7bb68f08763794f09072ab15
nicoaguilera18 <nicolasaguilera7914@gmail.com>
2026-06-17 23:54:51 -0300

    Delete src/supabase.ts

--- src/supabase.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-import { createClient } from '@supabase/supabase-js';
-
-// Usamos el ? para que no explote si import.meta.env no existe en GitHub Actions
-// Y le pasamos strings falsos de "mock" por si está corriendo pruebas automatizadas sin el .env
-const supabaseUrl = import.meta.env?.VITE_SUPABASE_URL || process.env.VITE_SUPABASE_URL || 'https://proyecto-prueba.supabase.co';
-const supabaseKey = import.meta.env?.VITE_SUPABASE_ANON_KEY || process.env.VITE_SUPABASE_ANON_KEY || 'llave-falsa-para-tests';
-
-export const supabase = createClient(supabaseUrl as string, supabaseKey as string);
