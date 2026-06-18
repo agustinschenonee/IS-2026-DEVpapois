da6e44e0f5279877b9c4d2a2812f748eb6019781
nicoaguilera18 <nicolasaguilera7914@gmail.com>
2026-06-18 00:32:04 -0300

    Delete tests/unit/validador.test.ts

--- tests/unit/validador.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, test, expect } from 'vitest';
-
-describe('Pruebas unitarias de DEVpapois', () => {
-  test('Verificar que el entorno de CI/CD reconoce TypeScript con Vitest', () => {
-    const proyecto: string = "DEVpapois";
-    expect(proyecto).toBe("DEVpapois");
-  });
-});
